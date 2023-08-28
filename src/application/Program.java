7f54d8b6ca7a69cb111da9505d64876541b13f8e
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-08-28 12:54:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,14 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+
 public class Program {
     public static void main(String[] args) {
-
+        Department obj=new Department(1,"Books");
+        Seller seller=new Seller(26,"Pedro","pedro@gmail.com", LocalDate.now(),3000.0,obj);
+        System.out.println(seller);
     }
 }
