c6e5b759db1836202e7239359b2af0ff48991dda
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-08-28 13:34:56 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.time.LocalDate;
@@ -9,5 +11,6 @@ public class Program {
         Department obj=new Department(1,"Books");
         Seller seller=new Seller(26,"Pedro","pedro@gmail.com", LocalDate.now(),3000.0,obj);
         System.out.println(seller);
+        SellerDao sellerDao= DaoFactory.createSellerDao();
     }
 }
