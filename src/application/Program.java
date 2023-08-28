cedd07a629d513f0452e0aa7546269874a47d8ac
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-08-28 14:22:42 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,16 @@
 package application;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj=new Department(1,"Books");
-        Seller seller=new Seller(26,"Pedro","pedro@gmail.com", LocalDate.now(),3000.0,obj);
-        System.out.println(seller);
+
         SellerDao sellerDao= DaoFactory.createSellerDao();
+        Seller seller=sellerDao.findById(3);
+        System.out.println(seller);
+        DB.closeConnection();
     }
 }
